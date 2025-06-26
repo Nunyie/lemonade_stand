a5c0ab257e2160638c08fe15b0b6a81d6c0d423b
Nunyie <roswellbunag05@gmail.com>
2025-06-26 20:23:32 +0800

    Privatized the profit value by adding 2 underscores, public can no longer access/modify the value.

--- lemonade_stand.py
+++ lemonade_stand.py
@@ -17,13 +17,16 @@ class LemonadeStand:
 
 class Cashier(LemonadeStand):
     def __init__(self):
-        self.profit = 100
+        self.__profit = 100 # Added 2 underscore to make the value private
     
     def total_order():
         print("Your total is $2.50")
 
     def price():
         print("The price for a Lemonade is $2.50")
+
+    def profit(self):
+        print("Total profit = ", self.__profit) # This method is used to access the private profit attribute
 
 # Inheritance: Cashier is child class of LemonadeStand.
 # It inherites the purchase() method from the LemonadeStand class. Which allows the cashier to call the purchase method since it is a subclass of LemonadeStand.
@@ -34,6 +37,12 @@ Cashier.total_order()
 
 # Polymorphism: The price method in the LemonadeStand class is overridden in the Cashier class to provide a different price for lemonade.
 
+# Encapsulation: The profit attribute in the Cashier class is private, meaning it cannot be accessed directly from outside the class.
+# The public can no longer access the profit attribute directly, ensuring that the profit value is protected.
+# Cashier = Cashier()
+# print("Total profit = ", Cashier.__profit)
+# print("Total profit = ", Cashier.__profit + 50)  # Adding $50 to the profit for demonstration
+
+# To access the Total profit we call the profit method
 Cashier = Cashier()
-print("Total profit = ", Cashier.profit)
-print("Total profit = ", Cashier.profit + 50)  # Adding $50 to the profit for demonstration
+Cashier.profit()
