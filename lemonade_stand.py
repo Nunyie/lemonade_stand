95ff1bddb26da79e6a198bfe4719b53c8caef65c
Nunyie <roswellbunag05@gmail.com>
2025-06-26 19:36:59 +0800

    Abstraction

--- lemonade_stand.py
+++ lemonade_stand.py
@@ -1,2 +1,10 @@
 # Nunyie
 # Demonstrating the 4 Pillars of OOP through a lemonade stand simulation
+
+print("Welcome to my Lemonade Stand")
+
+class LemonadeStand:
+    def purchase():
+        print("Here is your lemonade!")
+
+LemonadeStand.purchase()
