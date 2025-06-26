0577c6ff3d091a48ea0d215f9c5485833bb55a08
Nunyie <roswellbunag05@gmail.com>
2025-06-26 19:41:10 +0800

    Explained Abstraction

--- lemonade_stand.py
+++ lemonade_stand.py
@@ -7,4 +7,7 @@ class LemonadeStand:
     def purchase():
         print("Here is your lemonade!")
 
+# Abstraction: This is where we hide the process of making lemonade from the user.
+# The user only needs to know that they can purchase lemonade, not how it is made.
+
 LemonadeStand.purchase()
